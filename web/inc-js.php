c34643d591d85ba3d452ec7f5807c9654f33552a
Jim Wittig <jwittig@ucar.edu>
2026-06-16 16:48:23 -0600

    Initial versions of the files used for cron test jobs.

---
+++ web/inc-js.php
@@ -0,0 +1,4 @@
+<script src="/styles/um/js/jquery.min.js"></script>
+<!-- new js files -->
+<script src="/styles/um/js/bootstrap.bundle.min.js"></script>
+<script src="/styles/um/js/main.js"></script>
