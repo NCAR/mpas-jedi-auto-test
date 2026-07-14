8514102a5a27b01b21fbb1d7603198a7b9e54729
Jim Wittig <jwittig@ucar.edu>
2026-07-14 12:26:30 -0600

    change crontab to use a variable for the log file directory.

--- crontab.sh
+++ crontab.sh
@@ -4,6 +4,9 @@
 # 1. log on to cron.hpc.ucar.edu
 # 2. run: crontab crontab.sh
 
+# the base directory for log files
+log_dir=~/my_cron_logs
+
 # set this to the directory the mpas-bundle repository has been cloned to
 # don't put it on scratch or it will get corrupted due to scratch retention policy
 bundle_dir=/glade/work/jwittig/repos1/mpas-bundle-cron-src/mpas-bundle
@@ -39,11 +42,12 @@ webserver=eris.mmm.ucar.edu
 web_graphs_dir=/web/htdocs/projects/mpas-jedi/weekly-cycling/cylc_graphs
 
 # start at 11:05 PM and clean up log files
-05 23 * * 7 ssh $derecho "cd ~/my_cron_logs && (gunzip mpas-bundle-cron.log.tar.gz ; tar --remove-files -uf mpas-bundle-cron.log.tar mpas-bundle-cron.log.2* ; tar --remove-files -uf mpas-bundle-cron.log.tar git_shas* ; gzip mpas-bundle-cron.log.tar)"
+05 23 * * 5 ssh $derecho "cd $log_dir && (gunzip mpas-bundle-cron.log.tar.gz ; tar --remove-files -uf mpas-bundle-cron.log.tar mpas-bundle-cron.log.2* ; tar --remove-files -uf mpas-bundle-cron.log.tar git_shas* ; gzip mpas-bundle-cron.log.tar)"
 
-# at 7:30 AM on 7th day of the month clean up last months cylc log files
+# at 7:30 AM on 7th day of the month clean up last months log files
 lastmon=$(date --date='-1 month' +%Y-%m)
-30 7 7 * * ssh $derecho "cd ~/my_cron_logs/cylc/logs && tar --remove-files -czf run_cylc.cron.$lastmon.tgz *.$lastmon*.log"
+30 7 7 * * ssh $derecho "cd $log_dir/cylc/logs && tar --remove-files -czf run_cylc.cron.$lastmon.tgz *.$lastmon*.log"
+30 7 7 * * ssh $derecho "mv $log_dir/mpas-bundle-cron.log.tar.gz $log_dir/mpas-bundle-cron.log.$lastmon.tar.gz
 
 bld_suffix=date +%y_%0m_%0d
 
@@ -51,7 +55,7 @@ bld_suffix=date +%y_%0m_%0d
 # make a double precision build of mpas-bundle and run ctest
 # the build script won't do anything if there have been no changes to the
 # modules used to create mpas-bundle (unless the '-f' parameter is provided).
-05 00 * * 1-5 ssh $derecho "echo ${bld_suffix} ${bundle_dir} 'make 2p'  >> ~/my_cron_logs/cron.log && cd $bundle_dir && git fetch -p >> ~/my_cron_logs/cron.log ; git status >> ~/my_cron_logs/cron.log ; git pull >> ~/my_cron_logs/cron.log" ; $script_dir/$build_script -d $bundle_dir -b $builds_dir -q develop@desched1 -c gnu -p 2 -a nmmm0015
+05 00 * * 1-5 ssh $derecho "echo $(${bld_suffix}) ${bundle_dir} 'make 2p'  >> $log_dir/cron.log && cd $bundle_dir && git fetch -p >> $log_dir/cron.log ; git status >> $log_dir/cron.log ; git pull >> $log_dir/cron.log" ; $script_dir/$build_script -d $bundle_dir -b $builds_dir -q develop@desched1 -c gnu -p 2 -a nmmm0015
 
 # start at 12:05 AM on Sat 
 # always build and run ctests, even if no source change from previous run (-f)
@@ -70,16 +74,16 @@ bld_suffix=date +%y_%0m_%0d
 
 # at 12:05 am on Sun update the develop branch of  MPAS-Workflow repo and run the workflow
 suffix=$(date +%F)
-#05 00 * * 7 ssh $derecho "echo $suffix 'run workflow in ' $workflow_dir >> ~/my_cron_logs/cron.log && cd $workflow_dir && git fetch -p >> ~/my_cron_logs/cron.log 2>&1 && git co develop >> ~/my_cron_logs/cron.log 2>&1 && git pull >> ~/my_cron_logs/cron.log 2>&1 && $script_dir/$workflow_script -w $workflow_dir -d $bundle_build_dir -k $builds_dir/$build_script.lock -s $workflow_scenario -x $suffix"
-05 00 * * 7 ssh $derecho "echo $suffix 'run workflow in ' $workflow_dir >> ~/my_cron_logs/cron.log && cd $workflow_dir && $script_dir/$workflow_script -w $workflow_dir -d $bundle_build_dir -k $builds_dir/$build_script.lock -s $workflow_scenario -x $suffix"
+#05 00 * * 7 ssh $derecho "echo $suffix 'run workflow in ' $workflow_dir >> $log_dir/cron.log && cd $workflow_dir && git fetch -p >> $log_dir/cron.log 2>&1 && git co develop >> $log_dir/cron.log 2>&1 && git pull >> $log_dir/cron.log 2>&1 && $script_dir/$workflow_script -w $workflow_dir -d $bundle_build_dir -k $builds_dir/$build_script.lock -s $workflow_scenario -x $suffix -l $log_dir/cylc"
+05 00 * * 7 ssh $derecho "echo $suffix 'run workflow in ' $workflow_dir >> $log_dir/cron.log && cd $workflow_dir && $script_dir/$workflow_script -w $workflow_dir -d $bundle_build_dir -k $builds_dir/$build_script.lock -s $workflow_scenario -x $suffix -l $log_dir/cylc"
 # at 1:05 am on Sun run the 3dhybrid workflow
-05 01 * * 7 ssh $derecho "echo $suffix 'run workflow in ' $workflow_dir >> ~/my_cron_logs/cron.log && cd $workflow_dir && $script_dir/$workflow_script -w $workflow_dir -d $bundle_build_dir -k $builds_dir/$build_script.lock -s $workflow_scenario2 -x $suffix"
+05 01 * * 7 ssh $derecho "echo $suffix 'run workflow in ' $workflow_dir >> $log_dir/cron.log && cd $workflow_dir && $script_dir/$workflow_script -w $workflow_dir -d $bundle_build_dir -k $builds_dir/$build_script.lock -s $workflow_scenario2 -x $suffix -l $log_dir/cylc"
 
 # run the weekly cylc scenario using a gpu build of MPAS-Model
 suffix_gpu=2025-05-30_new
 workflow_scenario_gpu=scenarios/3denvar_OIE120km_WarmStart_VarBC_gpu_cron.yaml
-#55 15 * * * ssh $derecho "echo $suffix >> ~/my_cron_logs/cron.log 2>&1 && cd $workflow_dir && git fetch -p >> ~/my_cron_logs/cron.log 2>&1 && git co develop >> ~/my_cron_logs/cron.log 2>&1 && git pull >> ~/my_cron_logs/cron.log && $script_dir/$workflow_script -w $workflow_dir -d $bundle_build_dir -k $builds_dir/$build_script.lock -s $workflow_scenario_gpu -x ${suffix}_gpu"
+#55 15 * * * ssh $derecho "echo $suffix >> $log_dir/cron.log 2>&1 && cd $workflow_dir && git fetch -p >> $log_dir/cron.log 2>&1 && git co develop >> $log_dir/cron.log 2>&1 && git pull >> $log_dir/cron.log && $script_dir/$workflow_script -w $workflow_dir -d $bundle_build_dir -k $builds_dir/$build_script.lock -s $workflow_scenario_gpu -x ${suffix}_gpu -l $log_dir/cylc"
 
 # at 2:05 am Mon-Fri try to graph results from the completed workflow runs
 casper=casper.hpc.ucar.edu
-05 02 * * 1-5 ssh $casper "$script_dir/$workflow_script -w $workflow_dir -g $graphs_dir -o $graphs_out_dir -m $webserver -c $web_graphs_dir/" -e jwittig@ucar.edu
+05 02 * * 1-5 ssh $casper "$script_dir/$workflow_script -w $workflow_dir -g $graphs_dir -o $graphs_out_dir -m $webserver -c $web_graphs_dir/ -e jwittig@ucar.edu -l $log_dir/cylc"
